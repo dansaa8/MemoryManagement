6f233354fd7015feeb10b3d59c0a0e909a1bf9fe
dan saa <dansaa8@protonmail.com>
2024-09-26 17:49:28 +0200

    Create a method that evaluates if a string is well formatted in regard to opening and closing parentheses:  '(), [], {}'.

--- SkalProj_Datastrukturer_Minne/Util.cs
+++ SkalProj_Datastrukturer_Minne/Util.cs
@@ -73,4 +73,46 @@ public static class Util
 
         return input;
     }
+
+    public static bool CheckParenthesis(string baseString)
+    {
+        Stack<char> stack = new();
+
+        // Loopa över varje bokstav i inskickade strängen
+        foreach (char c in baseString)
+        {
+            // Om bokstaven innehåller en öppnande parentes, pushar vi in den överst i stacken
+            if (c == '(' || c == '{' || c == '[')
+            {
+                stack.Push(c);
+            }
+            // Om vi får en stängande parentes, så kommer det här else if statement att exekvera 
+            else if (c == ')' || c == '}' || c == ']')
+            {
+                // Vi kollar om det för närvarande finns några element i stacken.
+                // Om det inte gör det så betyder det att vi har en stängande
+                // parentes som inte har en startande motsvarighet.
+                if (stack.Count == 0) return false; 
+
+                // Vi plockar ut den senast tillagda öppnande parentesen ur stacken.
+                char top = stack.Pop();
+                
+                // Om den senast tillagda öppnande parentesen i stacken inte motsvarar
+                // den stängande parentesen, så betyder det att det finns
+                // en öppnande parentes av en annan typ som inte är stängd.
+                // Är så fallet, så är strängen som vi evaluerar inte "välformad"
+                // och vi returnerar false från metoden.
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        }
+
+        // Om stacken är tom så betyder det att alla öppnande parenteser har stängts
+        // Finns det öppnande parenteser kvar, så betyder det att inte alla har stängts.
+        // Nedanstående return uttryck nedanför kommer att resultera i ett bool-värde.
+        // Om det uttrycket stämmer så returnerar metoden true, annars false.
+        return stack.Count == 0;
+    }
+
 }
